97c8aadd10de569a7871f1f282887c975c811d6c
洪志明 <gi.ming.hong@gmail.com>
2017-12-27 14:19:54 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      {{ buyType.label }} - {{ buyType.value}}
+    </div>
+    <div>
+        <Selection :selections="buyTypes" @on-change="onParamChange('buyType', $event)"></Selection>
+    </div>
+    <!-- <HelloWorld/> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+import Selection from './components/base/selection'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      buyType: {},
+      buyTypes: [
+        {
+          label: '入門版',
+          value: 0
+        },
+        {
+          label: '中級版',
+          value: 1
+        },
+        {
+          label: '高級版',
+          value: 2
+        }
+      ]
+    }
+  },
+  components: {
+    // HelloWorld,
+    Selection
+  },
+  methods: {
+    onParamChange (attr, val) {
+      this[attr] = val
+      console.log(attr)
+      console.log(val)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
